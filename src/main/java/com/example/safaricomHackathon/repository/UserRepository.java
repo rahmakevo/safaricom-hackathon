c18d8dfc470426456b299d961cb4d12c0390a1b1
rahmakevo <siundu344@gmail.com>
2019-05-10 12:46:28 +0300

    refactor code

--- src/main/java/com/example/safaricomHackathon/repository/UserRepository.java
+++ src/main/java/com/example/safaricomHackathon/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.safaricomHackathon.repository;
 import com.example.safaricomHackathon.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface UserRepository extends CrudRepository<UserModel, Integer> {
-    List<UserModel> findByUserName(String username);
 }
