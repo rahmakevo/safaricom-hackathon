57c700f119e632d12e2ab7fe3928f1e4ac49d6c3
rahmakevo <siundu344@gmail.com>
2019-05-10 11:52:53 +0300

    refactor code

--- src/main/java/com/example/safaricomHackathon/repository/MovieRepository.java
+++ src/main/java/com/example/safaricomHackathon/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package com.example.safaricomHackathon.repository;
 
 import com.example.safaricomHackathon.model.MovieModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends CrudRepository<MovieModel, Integer> {
 }
