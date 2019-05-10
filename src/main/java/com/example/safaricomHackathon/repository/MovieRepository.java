ba52ccbf06ff275a8d39bd9b9edbf0ab19c71746
rahmakevo <siundu344@gmail.com>
2019-05-10 11:38:34 +0300

    working clean on master

--- src/main/java/com/example/safaricomHackathon/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.safaricomHackathon.repository;
-
-import com.example.safaricomHackathon.model.MovieModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepository extends CrudRepository<MovieModel, Long> {
-}
--- src/main/java/com/example/safaricomHackathon/repository/RegisterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.safaricomHackathon.repository;
-
-import com.example.safaricomHackathon.model.UserModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RegisterRepository extends CrudRepository<UserModel, Long> {
-
-}
