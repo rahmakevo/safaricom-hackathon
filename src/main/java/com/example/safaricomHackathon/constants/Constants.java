a65518459aa6f1400334117eebdb757ffafecd84
rahmakevo <siundu344@gmail.com>
2019-05-10 14:42:46 +0300

    winding up on jwt token

---
+++ src/main/java/com/example/safaricomHackathon/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.example.safaricomHackathon.constants;
+
+public class Constants {
+    public String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJrc2l1bmR1IiwiaWF0IjoxNTU3NDg3MjQ5LCJzdWIiOiJyZWdpc3RlciIsImlzcyI6ImtzaXVuZHUiLCJleHAiOjE1NTc0ODcyODV9.v5r_0hhBnaKe1ATPdacxKfiFWXrXd5HLQ9loAfFNhIQ";
+}
--- src/main/java/com/example/safaricomHackathon/controller/MovieController.java
+++ src/main/java/com/example/safaricomHackathon/controller/MovieController.java
@@ -1,5 +1,6 @@
 package com.example.safaricomHackathon.controller;
 
+import com.example.safaricomHackathon.constants.Constants;
 import com.example.safaricomHackathon.model.MovieModel;
 import com.example.safaricomHackathon.repository.MovieRepository;
 import com.google.gson.JsonObject;
@@ -13,11 +14,20 @@ public class MovieController {
     private MovieRepository movieRepository;
 
     @PostMapping("save/movies")
-    public String saveMovies(MovieModel movieModel) {
+    public String saveMovies(@RequestBody String accessToken, MovieModel movieModel) {
         JsonObject mResponseObject = new JsonObject();
-        movieRepository.save(movieModel);
-        mResponseObject.addProperty("status", "200");
-        mResponseObject.addProperty("message", "Data has been saved successfully");
+        Constants mConstant = new Constants();
+        String token = mConstant.token;
+        if (token.equals(accessToken)) {
+            movieRepository.save(movieModel);
+            mResponseObject.addProperty("status", "200");
+            mResponseObject.addProperty("message", "Data has been saved successfully");
+        } else {
+            mResponseObject.addProperty("status", "200");
+            mResponseObject.addProperty("message", "You do not have access to this service");
+        }
+
+
         return String.valueOf(mResponseObject);
     }
 
